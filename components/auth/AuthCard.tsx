9a5bc194ea2b138fcebc11bb3ad105778fc82f8a
soulc <1012875336@qq.com>
2026-01-08 17:03:29 +0800

    fix: remove annotation

--- components/auth/AuthCard.tsx
+++ components/auth/AuthCard.tsx
@@ -2,9 +2,6 @@
 
 import { motion } from "framer-motion";
 
-//todo 什么时候需要加default什么时候不需要
-//todo Framer Motion学习+motion.div用法
-//todo 下面涉及tailwind+css的知识点
 export default function AuthCard({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
