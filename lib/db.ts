9969699886eed17a37674463bc5b5f6c0e3a77a2
soulc <1012875336@qq.com>
2026-01-08 17:05:08 +0800

    fix: mongoDB connect state optimize

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,19 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-    throw new Error("❌ MONGODB_URI is not defined");
+  throw new Error("❌ MONGODB_URI is not defined");
 }
 
 export const connectDB = async () => {
-    // 判断连接状态，避免重复连接数据库
-    //todo >=1是否合理
-    if (mongoose.connections[0].readyState >= 1) {
-        console.log('MongoDB connected');
-        return;
-    }
-    (await mongoose.connect(MONGODB_URI));
-}
+  if (mongoose.connection.readyState === 1) {
+    console.log("MongoDB connected");
+    return;
+  }
+  if (mongoose.connection.readyState === 2) {
+    console.log("MongoDB connecting");
+    return;
+  }
+  await mongoose.connect(MONGODB_URI);
+};
